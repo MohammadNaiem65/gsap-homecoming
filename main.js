3e54b58fbec7240a13f1a30f0402e3ff8849372a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-11-03 03:57:45 +0600

    add: scroll triggered horizontal text animation

--- main.js
+++ main.js
@@ -4,40 +4,11 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 // register scroll trigger
 gsap.registerPlugin(ScrollTrigger);
 
-// page 1 animation
-gsap.fromTo(
-    '.page-1 .box',
-    {
-        scale: 0,
-        opacity: 0,
+gsap.to('.page-2 h1', {
+    transform: 'translateX(-80%)',
+    scrollTrigger: {
+        trigger: '.page-2',
+        scrub: 1,
+        pin: true,
     },
-    {
-        scale: 1,
-        opacity: 1,
-        rotate: 360,
-        duration: 2,
-    }
-);
-
-// page 2 animation
-gsap.fromTo(
-    '.page-2 .box',
-    {
-        scale: 0,
-        opacity: 0,
-    },
-    {
-        scale: 1,
-        opacity: 1,
-        rotate: 360,
-        duration: 2,
-        scrollTrigger: {
-            trigger: '.page-2 .box',
-            start: 'top 90%',
-            end: 'top 10%',
-            // toggleActions: 'play reverse play reverse', // Events to handle the animation.
-            scrub: 1,
-            markers: true,
-        },
-    }
-);
+});
